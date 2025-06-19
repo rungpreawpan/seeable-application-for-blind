e34d59d0deaf308458be86651888370d0184da3d
Preawpan Siriphalangkanont <87581600+rungpreawpan@users.noreply.github.com>
2025-06-19 21:24:06 +0700

    refactor object detection

---
+++ lib/views/object_detection/controller/object_detection_controller.dart
@@ -0,0 +1,82 @@
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:get/get.dart' hide Response, FormData, MultipartFile;
+import 'package:seeable/service/request_service.dart';
+import 'package:seeable/utils/alert.dart';
+import 'package:seeable/views/object_detection/model/object_detection_model.dart';
+
+class ObjectDetectionController extends GetxController {
+  var isLoading = false.obs;
+
+  ObjectDetectionModel? objectDetected;
+
+  uploadObject(File object) async {
+    bool isOnline = await RequestService().checkInternetConnection();
+
+    if (!isOnline) {
+      showAlert('ไม่มีสัญญาณอินเตอร์เน็ต');
+      isLoading.value = false;
+
+      return;
+    }
+
+    try {
+      isLoading.value = true;
+
+      var reqData = {
+        "image":
+            MultipartFile.fromFileSync(object.path, filename: 'object_image'),
+      };
+
+      FormData formData = FormData.fromMap(reqData);
+
+      var response = await RequestService().request(
+        '/upload-object',
+        method: HttpMethod.post,
+        data: formData,
+      );
+
+      if (response != null && response.statusCode == 200) {
+        await objectResults();
+      }
+    } catch (e) {
+      log(e.toString());
+    } finally {
+      isLoading.value = false;
+    }
+  }
+
+  objectResults() async {
+    bool isOnline = await RequestService().checkInternetConnection();
+
+    if (!isOnline) {
+      showAlert('ไม่มีสัญญาณอินเตอร์เน็ต');
+      isLoading.value = false;
+
+      return;
+    }
+
+    try {
+      isLoading.value = true;
+
+      var response = await RequestService().request(
+        '/object-results',
+        method: HttpMethod.get,
+      );
+
+      if (response != null && response.statusCode == 200) {
+        Map<String, dynamic> dataMap = response.data;
+        Map<String, dynamic> dataJSON = dataMap['objects'];
+        log(dataJSON.toString());
+
+        objectDetected = ObjectDetectionModel.fromJSON(dataJSON);
+      }
+    } catch (e) {
+      log(e.toString());
+    } finally {
+      isLoading.value = false;
+    }
+  }
+}
---
+++ lib/views/object_detection/model/object_detection_model.dart
@@ -0,0 +1,49 @@
+class ObjectDetectionModel {
+  List<BoxesModel>? boxes;
+  int? imageWidth;
+  int? imageHeight;
+
+  ObjectDetectionModel({
+    this.boxes,
+    this.imageWidth,
+    this.imageHeight,
+  });
+
+  factory ObjectDetectionModel.fromJSON(Map<String, dynamic> json) {
+    return ObjectDetectionModel(
+      boxes:
+          List.from(json['boxes']).map((e) => BoxesModel.fromJSON(e)).toList(),
+      imageWidth: int.parse(json['image_width'].toString()),
+      imageHeight: int.parse(json['image_height'].toString()),
+    );
+  }
+}
+
+class BoxesModel {
+  String? label;
+  double? confidence;
+  double? x1;
+  double? y1;
+  double? x2;
+  double? y2;
+
+  BoxesModel({
+    this.label,
+    this.confidence,
+    this.x1,
+    this.y1,
+    this.x2,
+    this.y2,
+  });
+
+  factory BoxesModel.fromJSON(Map<String, dynamic> json) {
+    return BoxesModel(
+      label: json['label'],
+      confidence: json['confidence'],
+      x1: double.parse(json['x1'].toString()),
+      y1: double.parse(json['y1'].toString()),
+      x2: double.parse(json['x2'].toString()),
+      y2: double.parse(json['y2'].toString()),
+    );
+  }
+}
